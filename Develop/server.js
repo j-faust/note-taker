1771ad8d95f601cc8b591321197e00182f26a825
j-faust <85812553+j-faust@users.noreply.github.com>
2022-12-20 22:58:40 -0500

    added notes to code

--- Develop/server.js
+++ Develop/server.js
@@ -4,7 +4,7 @@ const path = require('path');
 const express = require('express');
 
 
-const db = ('./db/db');
+const db = ('./db/db.json');
 
 const app = express();
 const PORT = process.env.PORT || 3001; 
@@ -41,32 +41,38 @@ app.route('/api/notes')
     .post((req, res, next) => {
         const { title, text } = req.body;
         // if note has title and text then posting to db
-        if(title && textarea) {
-            const newNoteText = {
+        let data = fs.readFileSync(db);
+        console.log(data);
+        let notes = JSON.parse(data);    
+    //   if statement ot add object to db
+        if(title && text) {
+            const noteText = {
                 title,
                 text,
                 id: dbId,
             };
-            fs.readFile('db.json', 'utf8', (err, data) => {
-                let notes = JSON.parse(data);
-                notes.push(newNoteText);
-                    fs.writeFile("db.json", JSON.stringify(notes), (err) => {
-                        if(err) {
-                         console.log(err);
-                     } else {
-                          console.log("Note Saved!");
-                   }
-             });    
+            // pushing notes into database
+             notes.push(db);
+            //  writing db file with new notes that are added
+                fs.writeFile("db.json", JSON.stringify(notes), (err) => {
+                    if(err) {
+                        console.log(err);
+                    } else {
+                        console.log("Note Saved!");
+                }   
             });
+            // response that note is save with note text for obj body
             const resp = {
                 status: "Note Saved!",
-                body: newNote,
+                body: noteText,
             };
+            // good response code
             res.status(200).json(resp);
         } else {
+            // response code for error
             res.status(500).json("Not not saved!");    
-        }
-
+        };
+        res.json(noteText);  
     });
 
 
--- README.md
+++ README.md
@@ -10,4 +10,6 @@ The purpose of this project is to have an application to be able to write and sa
 This project provided starter code, the server.js file was added and Express.js was used to help with utilizing the db.json file to show saved notes and to delete notes from the note taker app page.  
 
 The following was done within the server.js file using express.js - 
-    * 
+    * Utilized fs, uuid, path libraries
+    * Added funtionality for server via app.route function
+    *
