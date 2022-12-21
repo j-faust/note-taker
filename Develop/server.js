9276827afaf0b5a20d88cf824e742ee83ba81b37
j-faust <85812553+j-faust@users.noreply.github.com>
2022-12-20 22:16:24 -0500

    added code for app.post and routing for server.js file

--- Develop/server.js
+++ Develop/server.js
@@ -1,41 +1,76 @@
-// requiring packages 
+// requiring packages and setting variables
 const fs = require('fs');
 const path = require('path');
 const express = require('express');
 
-// setting db path to variable
-const dbData = ('./db/db.json');
+
+const db = ('./db/db');
 
 const app = express();
 const PORT = process.env.PORT || 3001; 
 
+// const for unique id for POST to db
+const uuid = require('uuid');
+const { response } = require('express');
+const dbId = uuid.v4;
+
 // accessing public folder
-app.use(express.static('public'));
+app.use(express.static('/public'));
 
 // middleware using urlencoded and express.json to parse incoming requests
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-
 // app.get requests for note and index
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'))
+    res.sendFile(path.join(__dirname, '/public/index.html'));
 });
 
 app.get('/notes', (req, res) => {
-    res.sendFile(path.join(__dirname, 'notes.html'))
+    res.sendFile(path.join(__dirname, '/public/notes.html'));
 });
 
+
+app.route('/api/notes')
 // returning notes from db file as json
-app.get('/api/notes', (req, res) => res.json(dbData));
+    .get((req, res, next) => {
+        let data = fs.readFileSync(db);
+        let dbJSON = JSON.parse(data);
+        res.json(dbJSON);
+})
+    .post((req, res, next) => {
+        const { title, text } = req.body;
+        // if note has title and text then posting to db
+        if(title && textarea) {
+            const newNoteText = {
+                title,
+                text,
+                id: dbId,
+            };
+            fs.readFile('db.json', 'utf8', (err, data) => {
+                let notes = JSON.parse(data);
+                notes.push(newNoteText);
+                    fs.writeFile("db.json", JSON.stringify(notes), (err) => {
+                        if(err) {
+                         console.log(err);
+                     } else {
+                          console.log("Note Saved!");
+                   }
+             });    
+            });
+            const resp = {
+                status: "Note Saved!",
+                body: newNote,
+            };
+            res.status(200).json(resp);
+        } else {
+            res.status(500).json("Not not saved!");    
+        }
+
+    });
 
 
-
-
-
-
-
-
+// setting to listen to server
 app.listen(PORT, () => {
     console.log(`Server is listening at http://localhost:${PORT}`);
   });
--- README.md
+++ README.md
@@ -1,1 +1,13 @@
-# note-taker
+# Note Taker - Module 11 Challenge
+
+## Description 
+
+The purpose of this project is to have an application to be able to write and save notes to help with organizing thoughts and to keep track of to-do items.  The application will use express.js to help with the server and database functioning as well as html, javascript and node.js to make the app functional.  
+
+
+## Express
+
+This project provided starter code, the server.js file was added and Express.js was used to help with utilizing the db.json file to show saved notes and to delete notes from the note taker app page.  
+
+The following was done within the server.js file using express.js - 
+    * 
